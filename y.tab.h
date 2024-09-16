2ffb077d1859e48042da15f7ae5d8d65db528e2c
adv8j <advait.jain2003@gmail.com>
2024-09-16 22:52:04 +0530

    minor fix

--- y.tab.h
+++ y.tab.h
@@ -1,238 +1,84 @@
-/* A Bison parser, made by GNU Bison 3.8.2.  */
+#ifndef _yy_defines_h_
+#define _yy_defines_h_
 
-/* Bison interface for Yacc-like parsers in C
+#define ASSIGN 257
+#define PLUS 258
+#define MINUS 259
+#define MUL 260
+#define DIV 261
+#define MOD 262
+#define EQ 263
+#define NEQ 264
+#define GT 265
+#define LT 266
+#define GTE 267
+#define LTE 268
+#define AND 269
+#define OR 270
+#define NOT 271
+#define ADD_ASSIGN 272
+#define SUB_ASSIGN 273
+#define MUL_ASSIGN 274
+#define DIV_ASSIGN 275
+#define MOD_ASSIGN 276
+#define SEMICOLON 277
+#define COLON 278
+#define LPAREN 279
+#define RPAREN 280
+#define LBRACKET 281
+#define RBRACKET 282
+#define LBRACE 283
+#define RBRACE 284
+#define COMMA 285
+#define DOT 286
+#define RANGE 287
+#define RANGE_INCL 288
+#define REFERENCE 289
+#define TASKGROUP 290
+#define TASK 291
+#define PROPERTIES 292
+#define ORDER 293
+#define MEM 294
+#define SHARED_DIRECTIVE 295
+#define SUPERVISOR 296
+#define ALL 297
+#define UNSAFE 298
+#define JOIN 299
+#define CALL 300
+#define CHN_SEND 301
+#define CHANNEL_WAIT 302
+#define TASK_CHANNEL 303
+#define LOG 304
+#define ARROW 305
+#define PARALLEL 306
+#define PRIVATE 307
+#define SHARED 308
+#define SCHEDULE 309
+#define NUM_THREADS 310
+#define REDUCTION 311
+#define DYNAMIC_SCHEDULE 312
+#define STATIC_SCHEDULE 313
+#define MUT 314
+#define INT 315
+#define CHAR 316
+#define LONG 317
+#define BOOL 318
+#define FLOAT 319
+#define STRING 320
+#define FUNC 321
+#define RETURN 322
+#define STRUCT 323
+#define IF 324
+#define ELSE 325
+#define FOR 326
+#define IN 327
+#define TRUE 328
+#define FALSE 329
+#define IDENTIFIER 330
+#define SHARED_IDENTIFIER 331
+#define INT_LITERAL 332
+#define FLOAT_LITERAL 333
+#define STRING_LITERAL 334
+#define CHARACTER_LITERAL 335
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
-   Inc.
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
-
-/* As a special exception, you may create a larger work that contains
-   part or all of the Bison parser skeleton and distribute that work
-   under terms of your choice, so long as that work isn't itself a
-   parser generator using the skeleton or a modified version thereof
-   as a parser skeleton.  Alternatively, if you modify or redistribute
-   the parser skeleton itself, you may (at your option) remove this
-   special exception, which will cause the skeleton and the resulting
-   Bison output files to be licensed under the GNU General Public
-   License without this special exception.
-
-   This special exception was added by the Free Software Foundation in
-   version 2.2 of Bison.  */
-
-/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
-   especially those whose name start with YY_ or yy_.  They are
-   private implementation details that can be changed or removed.  */
-
-#ifndef YY_YY_Y_TAB_H_INCLUDED
-# define YY_YY_Y_TAB_H_INCLUDED
-/* Debug traces.  */
-#ifndef YYDEBUG
-# define YYDEBUG 0
-#endif
-#if YYDEBUG
-extern int yydebug;
-#endif
-
-/* Token kinds.  */
-#ifndef YYTOKENTYPE
-# define YYTOKENTYPE
-  enum yytokentype
-  {
-    YYEMPTY = -2,
-    YYEOF = 0,                     /* "end of file"  */
-    YYerror = 256,                 /* error  */
-    YYUNDEF = 257,                 /* "invalid token"  */
-    ASSIGN = 258,                  /* ASSIGN  */
-    PLUS = 259,                    /* PLUS  */
-    MINUS = 260,                   /* MINUS  */
-    MUL = 261,                     /* MUL  */
-    DIV = 262,                     /* DIV  */
-    MOD = 263,                     /* MOD  */
-    EQ = 264,                      /* EQ  */
-    NEQ = 265,                     /* NEQ  */
-    GT = 266,                      /* GT  */
-    LT = 267,                      /* LT  */
-    GTE = 268,                     /* GTE  */
-    LTE = 269,                     /* LTE  */
-    AND = 270,                     /* AND  */
-    OR = 271,                      /* OR  */
-    NOT = 272,                     /* NOT  */
-    ADD_ASSIGN = 273,              /* ADD_ASSIGN  */
-    SUB_ASSIGN = 274,              /* SUB_ASSIGN  */
-    MUL_ASSIGN = 275,              /* MUL_ASSIGN  */
-    DIV_ASSIGN = 276,              /* DIV_ASSIGN  */
-    MOD_ASSIGN = 277,              /* MOD_ASSIGN  */
-    SEMICOLON = 278,               /* SEMICOLON  */
-    COLON = 279,                   /* COLON  */
-    LPAREN = 280,                  /* LPAREN  */
-    RPAREN = 281,                  /* RPAREN  */
-    LBRACKET = 282,                /* LBRACKET  */
-    RBRACKET = 283,                /* RBRACKET  */
-    LBRACE = 284,                  /* LBRACE  */
-    RBRACE = 285,                  /* RBRACE  */
-    COMMA = 286,                   /* COMMA  */
-    DOT = 287,                     /* DOT  */
-    RANGE = 288,                   /* RANGE  */
-    RANGE_INCL = 289,              /* RANGE_INCL  */
-    REFERENCE = 290,               /* REFERENCE  */
-    TASKGROUP = 291,               /* TASKGROUP  */
-    TASK = 292,                    /* TASK  */
-    PROPERTIES = 293,              /* PROPERTIES  */
-    ORDER = 294,                   /* ORDER  */
-    MEM = 295,                     /* MEM  */
-    SHARED_DIRECTIVE = 296,        /* SHARED_DIRECTIVE  */
-    SUPERVISOR = 297,              /* SUPERVISOR  */
-    ALL = 298,                     /* ALL  */
-    UNSAFE = 299,                  /* UNSAFE  */
-    JOIN = 300,                    /* JOIN  */
-    CALL = 301,                    /* CALL  */
-    CHN_SEND = 302,                /* CHN_SEND  */
-    CHANNEL_WAIT = 303,            /* CHANNEL_WAIT  */
-    TASK_CHANNEL = 304,            /* TASK_CHANNEL  */
-    LOG = 305,                     /* LOG  */
-    ARROW = 306,                   /* ARROW  */
-    PARALLEL = 307,                /* PARALLEL  */
-    PRIVATE = 308,                 /* PRIVATE  */
-    SHARED = 309,                  /* SHARED  */
-    SCHEDULE = 310,                /* SCHEDULE  */
-    NUM_THREADS = 311,             /* NUM_THREADS  */
-    REDUCTION = 312,               /* REDUCTION  */
-    DYNAMIC_SCHEDULE = 313,        /* DYNAMIC_SCHEDULE  */
-    STATIC_SCHEDULE = 314,         /* STATIC_SCHEDULE  */
-    MUT = 315,                     /* MUT  */
-    INT = 316,                     /* INT  */
-    CHAR = 317,                    /* CHAR  */
-    LONG = 318,                    /* LONG  */
-    BOOL = 319,                    /* BOOL  */
-    FLOAT = 320,                   /* FLOAT  */
-    STRING = 321,                  /* STRING  */
-    FUNC = 322,                    /* FUNC  */
-    RETURN = 323,                  /* RETURN  */
-    STRUCT = 324,                  /* STRUCT  */
-    IF = 325,                      /* IF  */
-    ELSE = 326,                    /* ELSE  */
-    FOR = 327,                     /* FOR  */
-    IN = 328,                      /* IN  */
-    TRUE = 329,                    /* TRUE  */
-    FALSE = 330,                   /* FALSE  */
-    IDENTIFIER = 331,              /* IDENTIFIER  */
-    SHARED_IDENTIFIER = 332,       /* SHARED_IDENTIFIER  */
-    INT_LITERAL = 333,             /* INT_LITERAL  */
-    FLOAT_LITERAL = 334,           /* FLOAT_LITERAL  */
-    STRING_LITERAL = 335,          /* STRING_LITERAL  */
-    CHARACTER_LITERAL = 336        /* CHARACTER_LITERAL  */
-  };
-  typedef enum yytokentype yytoken_kind_t;
-#endif
-/* Token kinds.  */
-#define YYEMPTY -2
-#define YYEOF 0
-#define YYerror 256
-#define YYUNDEF 257
-#define ASSIGN 258
-#define PLUS 259
-#define MINUS 260
-#define MUL 261
-#define DIV 262
-#define MOD 263
-#define EQ 264
-#define NEQ 265
-#define GT 266
-#define LT 267
-#define GTE 268
-#define LTE 269
-#define AND 270
-#define OR 271
-#define NOT 272
-#define ADD_ASSIGN 273
-#define SUB_ASSIGN 274
-#define MUL_ASSIGN 275
-#define DIV_ASSIGN 276
-#define MOD_ASSIGN 277
-#define SEMICOLON 278
-#define COLON 279
-#define LPAREN 280
-#define RPAREN 281
-#define LBRACKET 282
-#define RBRACKET 283
-#define LBRACE 284
-#define RBRACE 285
-#define COMMA 286
-#define DOT 287
-#define RANGE 288
-#define RANGE_INCL 289
-#define REFERENCE 290
-#define TASKGROUP 291
-#define TASK 292
-#define PROPERTIES 293
-#define ORDER 294
-#define MEM 295
-#define SHARED_DIRECTIVE 296
-#define SUPERVISOR 297
-#define ALL 298
-#define UNSAFE 299
-#define JOIN 300
-#define CALL 301
-#define CHN_SEND 302
-#define CHANNEL_WAIT 303
-#define TASK_CHANNEL 304
-#define LOG 305
-#define ARROW 306
-#define PARALLEL 307
-#define PRIVATE 308
-#define SHARED 309
-#define SCHEDULE 310
-#define NUM_THREADS 311
-#define REDUCTION 312
-#define DYNAMIC_SCHEDULE 313
-#define STATIC_SCHEDULE 314
-#define MUT 315
-#define INT 316
-#define CHAR 317
-#define LONG 318
-#define BOOL 319
-#define FLOAT 320
-#define STRING 321
-#define FUNC 322
-#define RETURN 323
-#define STRUCT 324
-#define IF 325
-#define ELSE 326
-#define FOR 327
-#define IN 328
-#define TRUE 329
-#define FALSE 330
-#define IDENTIFIER 331
-#define SHARED_IDENTIFIER 332
-#define INT_LITERAL 333
-#define FLOAT_LITERAL 334
-#define STRING_LITERAL 335
-#define CHARACTER_LITERAL 336
-
-/* Value type.  */
-#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef int YYSTYPE;
-# define YYSTYPE_IS_TRIVIAL 1
-# define YYSTYPE_IS_DECLARED 1
-#endif
-
-
-extern YYSTYPE yylval;
-
-
-int yyparse (void);
-
-
-#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
+#endif /* _yy_defines_h_ */
